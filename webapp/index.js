175c6dbcf01d2667fd82e97b030e29c37f03e558
shubh-2012 <77185813+shubh-2012@users.noreply.github.com>
2024-05-12 20:24:25 +0530

    UI5 hello world

--- webapp/index.js
+++ webapp/index.js
@@ -1,6 +1,6 @@
-sap.ui5.define([
-    'require',
-    'dependency'
+sap.ui.define([
+    // 'require',
+    // 'dependency'
 ], function(require, factory) {
     'use strict';
     alert('Ui5 display ready');
