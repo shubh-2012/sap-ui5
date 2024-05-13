e123823cd033af97c93236ec05da7845af432d2b
shubh-2012 <77185813+shubh-2012@users.noreply.github.com>
2024-05-13 21:49:29 +0530

    sap ui5

---
+++ webapp/component.js
@@ -0,0 +1,38 @@
+sap.ui.define(
+  ["sap/ui/core/UIComponent", "sap/ui/model/json/JSONModel"],
+  function (UIComponent, JSONModel) {
+    "use strict";
+    return UIComponent.extend("sap.ui.sap-ui5.component", {
+      metadata: {
+        manifest: "json",
+        // rootView: {
+        //   viewName: "sap.ui.sap-ui5.view.App",
+        //   type: "XML",
+        //   async: true,
+        //   id: "app",
+        // },
+      },
+      init: function () {
+        // call the init function of parent
+        UIComponent.prototype.init.apply(this, arguments);
+        // set data models
+        var oData = {
+          recipient: {
+            name: "John",
+          },
+        };
+        var oModel = new JSONModel(oData);
+        this.setModel(oModel);
+
+        // set i18n model
+
+        // var i18nModel = new ResourceModel({
+        //   bundleName: "sap.ui.sap-ui5.i18n.i18n",
+        //   supportLocales: [""],
+        //   fallbackLocale: "",
+        // });
+        // this.setModel(i18nModel, "i18n");
+      },
+    });
+  }
+);
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,24 +1,25 @@
-sap.ui.define([
+sap.ui.define(
+  [
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
-],function(Controller,MessageToast,JSONModel){
-    "use strict"
-    return Controller.extend("sap.ui.sap-ui5.App",{
-        
-        onInit: function (){
-            var oData = {
-                recipient: {
-                    name: "John"
-                }
-            };
-            var oModel = new JSONModel(oData);
-            this.getView().setModel(oModel);
-        },
-
-        onShowHello: function(){
-            // alert("Hell there");
-            MessageToast.show("Hello There!");
-        }
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel",
+  ],
+  function (Controller, MessageToast) {
+    "use strict";
+    return Controller.extend("sap.ui.sap-ui5.App", {
+      onShowHello: function () {
+        // read msg from i18n model
+        var oBundle = this.getView().getModel("i18n").getResourceBundle();
+        var sRecipient = this.getView()
+          .getModel()
+          .getProperty("/recipient/name");
+        var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+        // alert("Hell there");
+        // Show Message
+        MessageToast.show(sMsg);
+        // MessageToast.show("Hello There!");
+      },
     });
-});
+  }
+);
